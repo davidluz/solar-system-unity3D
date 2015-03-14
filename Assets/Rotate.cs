217efc7682b19976c86a456582e665fcd9870a0a
davidluz <davidmelo8@hotmail.com>
2015-03-14 18:27:02 -0300

    v2.0

---
+++ Assets/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public GameObject terra;
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//terra.transform.Rotate((Vector3.up * Time.deltaTime)*10, Space.World);
+	
+	
+	}
+}
